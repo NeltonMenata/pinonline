f5d84a42b987c14ceaede428ea3f37d18d13bc18
Nelton Menata <neltonmenataditec@gmail.com>
2021-07-13 14:07:30 +0100

    Adição da tela de SplashScreen

---
+++ lib/app/app_views/app_components/animated_splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnimatedSplashScreen extends StatefulWidget {
+  const AnimatedSplashScreen({ Key? key }) : super(key: key);
+
+  @override
+  _AnimatedSplashScreenState createState() => _AnimatedSplashScreenState();
+}
+
+class _AnimatedSplashScreenState extends State<AnimatedSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
