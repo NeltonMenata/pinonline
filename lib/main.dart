31bae7dc0be3ea563b3202b3f50ebb7cec8787f8
neltonmenata <neltonmenataditec@gmail.com>
2021-08-17 14:31:40 +0100

    Permissão de Acesso a Internet na versão Release

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
-import 'app/app_provider/provider_data.dart';
 import 'app/app_views/app_routes/routes.dart';
 import 'package:get/get.dart';
 
@@ -10,11 +9,9 @@ void main() async {
   final keyApplicationId = "OsiPUlfwC4Av3KtTAlxO5HssYxdeH9g8OrgSBxGu";
   final keyClientKey = "uDLx5YOIKES5TUXuzTUt8WAM4jgwOq5QjF3wAiIP";
   final keyParseServerUrl = "https://parseapi.back4app.com";
-  ProviderData();
+  
   await Parse().initialize(keyApplicationId, keyParseServerUrl, clientKey: keyClientKey, autoSendSessionId: true);
-  
-  print("Conexao feita com sucesso");
-  
+
   runApp(MyApp());
 }
 
