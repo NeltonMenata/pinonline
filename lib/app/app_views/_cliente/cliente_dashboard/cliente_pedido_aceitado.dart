37ea41a1e69e240532fa3d89e231be727a33f113
neltonmenata <neltonmenataditec@gmail.com>
2021-09-07 13:08:46 +0100

    DashBoard - Pedidos Aceitas e Pedidos Recibo 1.1

--- lib/app/app_views/_cliente/cliente_dashboard/cliente_pedido_aceitado.dart
+++ lib/app/app_views/_cliente/cliente_dashboard/cliente_pedido_aceitado.dart
@@ -4,14 +4,12 @@ import 'package:pinonline/app/app_controller/_cliente/cliente_dashboard/cliente_
 import 'components/obra_component_view.dart';
 
 class ClientePedidoAceitado extends StatelessWidget {
-
   final _controller = ClienteDashBoardController.controller;
-  
 
   @override
   Widget build(BuildContext context) {
     _controller.dashBoardFresh();
-    
+
     return Center(
       child: Container(
         color: Colors.blue,
@@ -38,15 +36,48 @@ class ClientePedidoAceitado extends StatelessWidget {
       ),
     );
   }
-  _confirmar(BuildContext context){
-    showDialog(context: context, builder: (context){
-      return AlertDialog(
-        title: Text("JustBuild"),
-        content: Text("Vosso contracto foi confirmado com sucesso para ambas as partes!"),
-        actions: [
-          TextButton(onPressed: (){Navigator.pop(context);}, child: Text("OK"))
-        ],
-      );
-    });
+
+  _confirmar(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("JustBuild"),
+            content: Text("Tens certeza que aceitas a contra-proposta?"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text(
+                            "JustBuild",
+                            style: TextStyle(color: Colors.green),
+                          ),
+                          content: Text(
+                              "Vosso contracto foi confirmado com sucesso para ambas as partes!"),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("OK"))
+                          ],
+                        );
+                      });
+                },
+                child: Text("Sim"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Não"),
+              ),
+            ],
+          );
+        });
   }
 }
